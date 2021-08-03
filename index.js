11314706603e3d4887e7e1bc7e7291e0f2b55bdf
IAmRC1 <rishabhcena1@gmail.com>
2021-08-03 13:29:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "normalize.css"
+import "./src/assets/styles/custom.css"
+import App from "./src/components/App";
+
+ReactDom.render(<App />, document.getElementById("app"));
